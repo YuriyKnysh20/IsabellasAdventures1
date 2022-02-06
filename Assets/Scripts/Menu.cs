9581d4f2f2019b2af3a0f2d488cc1ed7b5dee8ea
Flowqq <60794210+Flowqq@users.noreply.github.com>
2022-02-06 23:06:01 +0300

    Добавил выбор персонажей
    +починил работу фонов

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,15 +5,28 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private InputField nameField;
+    [SerializeField] private GameObject characterScreen;
 	public Button back;
 	private void Start()
-	{		
-		if(PlayerPrefs.HasKey("Player_Name"))
+	{
+        characterScreen.SetActive(false);
+
+        if (PlayerPrefs.HasKey("Player_Name"))
 		nameField.text=PlayerPrefs.GetString("Player_Name");
 	}
 	public void  OnEndEditName()
 	{
 		PlayerPrefs.SetString("Player_Name",nameField.text);
+    }
+
+	public void LoadCharacterChoiceScreen()
+    {
+        characterScreen.SetActive(true);
+    }
+
+	public void LoadCharacterScreen()
+    {
+
     }
 
     public void LoadTo(int level)
